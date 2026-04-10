424d80f7b9f177e8ad29a0377cc2019337552960
Jithendra Pulusu <82011691+jithendrapulusu28@users.noreply.github.com>
2026-04-10 19:04:33 +0530

    added new column OrderChannel

--- datawarehouse_test.Warehouse/dbo/Tables/Orders.sql
+++ datawarehouse_test.Warehouse/dbo/Tables/Orders.sql
@@ -9,5 +9,6 @@ CREATE TABLE [dbo].[Orders] (
 	[TotalAmount] decimal(12,2) NULL, 
 	[Region] varchar(50) NULL, 
 	[Status] varchar(20) NULL, 
-	[OrderSource] varchar(30) NULL
+	[OrderSource] varchar(30) NULL, 
+	[OrderChannel] varchar(30) NULL
 );
